52c8fac48c787920ec3e61ae60c7f5cf7f1a10f3
root <root@DESKTOP-9DQ67BP.localdomain>
2018-07-30 11:37:35 -0700

    more stuff

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import webapp2
 import os
 import jinja2
 
-current_jinja_environment = JINJA_ENVIRONMENT = jinja2.Environment(
+current_jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write("Hello")
+        self.response.write("Hello, welcome to FreshFridge!")
+        home_template = current_jinja_environment.get_template('/templates/home.html')
+        self.response.write(home_template.render())
 
 
 
